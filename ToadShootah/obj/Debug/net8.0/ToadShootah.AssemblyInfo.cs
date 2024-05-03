5e321eb7b34d004451c99b72d2f9b6def809303a
lil-hummonkey <hector.mcewen@elev.ga.ntig.se>
2024-05-03 10:09:36 +0200

    yuhhh8

--- ToadShootah/obj/Debug/net8.0/ToadShootah.AssemblyInfo.cs
+++ ToadShootah/obj/Debug/net8.0/ToadShootah.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToadShootah")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d0a458c32ee215ebddf1674ef3ff97b4ab231e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c22d4e83ede9bb2f572679beb2dfed2e8f50c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToadShootah")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToadShootah")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
